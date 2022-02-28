548c020f8ce2294dc28d72b3254ed9f2c75e8aea
Reem <janinareem@gmail.com>
2022-02-28 21:55:54 +0200

    Add the routes to myProfilw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MyProfile from './components/MyProfile';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Dragons from './components/Dragons';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>I still work</h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/Missions" element={<Missions />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
+          <Route path="/Dragons" element={<Dragons />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
