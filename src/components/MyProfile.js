205d57d0b59a85f1a43ab6a107e3b89a7ff53eef
Fabricio Garcia <fabgrel10@gmail.com>
2022-02-28 14:52:26 -0600

    Create structure for MyProfile component and added style

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,17 @@
+import '../style/MyProfile.css';
+
 const MyProfile = () => (
-  <h2>Hello from MyProfile</h2>
+  <div className="my-profile__container">
+    <div className="my-profile__rockets">
+      <h2>My Rockets</h2>
+    </div>
+    <div className="my-profile__missions">
+      <h2>My Missions</h2>
+    </div>
+    <div className="my-profile__dragons">
+      <h2>My Dragons</h2>
+    </div>
+  </div>
 );
 
 export default MyProfile;
