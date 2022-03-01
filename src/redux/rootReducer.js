b0285df11b495dbe7ff2afc54fe45ca4df0dc591
Reem <janinareem@gmail.com>
2022-03-02 01:36:18 +0200

    Add the rocket reducers

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import missionsReducer from './reducers/missionsReducer';
+import RocketsReducer from './reducers/RocketsReducer';
 
 const rootReducer = combineReducers({
   missions: missionsReducer,
+  RocketsReducer,
 });
 
 export default rootReducer;
