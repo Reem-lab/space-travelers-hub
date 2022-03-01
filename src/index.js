c117164c01d8bc2dcfcb2fb92cfeb31bf58ce596
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-01 15:59:10 -0600

    Import mdbreact to align text using bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} />
