bcbf2fbcd842878f7aba3994f82cf3cc9222aadb
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-01 10:15:10 -0600

    Create structure for Missions component
    - Create basic structire for Missions
    - Fetch data from the Missions endpoint
    - Display missions
    Resolves #18 and #20
    References #15

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,38 @@
-const Missions = () => (
-  <h2>Hello from missions</h2>
-);
+import { useEffect, useState } from 'react';
+import '../style/Missions.css';
+import Mission from './Mission';
+
+const Missions = () => {
+  const [missions, setMissions] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.spacexdata.com/v3/missions');
+      const data = await response.json();
+      setMissions(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log('missions: ', missions[0]);
+
+  return (
+    <>
+      <div className="headers-container">
+        <div className="column-mission">Mission</div>
+        <div className="column-description">Description</div>
+        <div className="column-status">Status</div>
+        <div className="column-status-2">Status2 </div>
+      </div>
+      {missions.map((mission) => (
+        <Mission
+          key={mission.mission_id}
+          name={mission.mission_name}
+          description={mission.description}
+        />
+      ))}
+    </>
+  );
+};
 
 export default Missions;
