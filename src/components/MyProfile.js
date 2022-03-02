b82c3ffbc95e9cd7ca880be14573096e3eea328a
Reem <janinareem@gmail.com>
2022-03-02 23:55:08 +0200

    Add functionality to make the filteration

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,12 +3,19 @@ import '../style/MyProfile.css';
 
 const MyProfile = () => {
   const myMissions = useSelector((state) => state.missions);
-  console.log(myMissions);
+  const rockets = useSelector((state) => state.RocketsReducer);
 
   return (
     <div className="my-profile__container">
       <div className="my-profile__rockets">
         <h2>My Rockets</h2>
+        {rockets.map((rocket) => (
+          rocket.reserved && (
+            <div className="myProf-rocket" key={rocket.id}>
+              <p>{rocket.rocket_name}</p>
+            </div>
+          )
+        ))}
       </div>
       <div className="my-profile__missions">
         <h2>My Missions</h2>
