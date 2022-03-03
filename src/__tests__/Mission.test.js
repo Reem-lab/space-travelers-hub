6108ef4c159d26a4455b892ddf7d1466b2ec9c40
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-03 15:13:19 -0600

    Run linters checks

--- src/__tests__/Mission.test.js
+++ src/__tests__/Mission.test.js
@@ -5,9 +5,6 @@ import { Provider } from 'react-redux';
 import Mission from '../components/Mission';
 import store from '../redux/store';
 
-const setMission = () => {
-  return '';
-}
 
 const missions = [
   {
@@ -16,7 +13,6 @@ const missions = [
     description:
       'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
     reserved: false,
-    toggleMission: setMission(),
   },
 ];
 
@@ -36,7 +32,6 @@ const mockObj = () => {
               description={mission.description}
               id={mission.mission_id}
               reserved={mission.reserved}
-              toggleMission={setMission}
             />
           ))}
         </tbody>
