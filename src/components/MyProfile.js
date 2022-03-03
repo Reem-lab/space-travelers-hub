57eff388da9fe6e024fe0873b243aaf2a6911191
Reem <janinareem@gmail.com>
2022-03-03 22:01:55 +0200

    Fix lintets

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,46 +1,47 @@
 import { useSelector } from 'react-redux';
 import '../style/MyProfile.css';
-
 const MyProfile = () => {
-  const myMissions = useSelector((state) => state.missions);
-  const myDragons = useSelector((state) => state.dragons);
-  const rockets = useSelector((state) => state.RocketsReducer.filter((rock) => rock.reserved));
-
+  const missions = useSelector((state) => {
+    const { missions } = state;
+    return missions.filter((missions) => missions.reserved);
+  });
+  const rockets = useSelector((state) => {
+    const { RocketsReducer } = state;
+    return RocketsReducer.filter((rockets) => rockets.reserved);
+  });
+  const dragons = useSelector((state) => {
+    const { dragons } = state;
+    return dragons.filter((dragons) => dragons.reserved);
+  });
   return (
     <div className="my-profile__container">
       <div className="my-profile__rockets">
         <h2>My Rockets</h2>
-        {
-    rockets.map((rocket) => (
-      <div key={rocket.id}>
-        <p>{rocket.rocket_name}</p>
-      </div>
-    ))
-}
-
+        {rockets.map((rocket) => (
+          rocket.reserved && (
+          <div key={rocket.id}>
+            <p>{rocket.rocket_name}</p>
+          </div>
+          )
+        ))}
       </div>
       <div className="my-profile__missions">
         <h2>My Missions</h2>
-        {myMissions.map((mission) => (
-          mission.reserved && (
-            <div key={mission.mission_name}>
-              <p>{mission.mission_name}</p>
-            </div>
-          )
+        {missions.map((mission) => (
+          <div key={mission.mission_name}>
+            <p>{mission.mission_name}</p>
+          </div>
         ))}
       </div>
       <div className="my-profile__dragons">
         <h2>My Dragons</h2>
-        {myDragons.map((dragon) => (
-          dragon.reserved && (
-            <div key={dragon.id}>
-              <p>{dragon.name}</p>
-            </div>
-          )
+        {dragons.map((dragon) => (
+          <div key={dragon.id}>
+            <p>{dragon.name}</p>
+          </div>
         ))}
       </div>
     </div>
   );
 };
-
 export default MyProfile;
