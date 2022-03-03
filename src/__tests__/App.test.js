5fc84b72abde6a0e9889ad94f687d34f4dc6d048
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-03 14:19:12 -0600

    Test dragons section rendering on click of navlink

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+import { Provider } from 'react-redux';
+import store from "../redux/store";
+
+describe('Test Router behaviour', () => {
+  it('When Dragons in navbar is clicked, it renders the Dragons Section', () => {
+    //ARRANGE
+
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+    //ACT
+    const dragonsLink = screen.getByText('Dragons');
+    fireEvent.click(dragonsLink);
+
+    //ASSERT
+    const body = container.querySelector('body');
+    expect(body).toMatchSnapshot();
+  });
+});
